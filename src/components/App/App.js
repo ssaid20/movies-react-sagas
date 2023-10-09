78546dffdfaa95332cfe7717284901392d2616e4
Said Said <ssaid20@icloud.com>
2023-10-08 19:56:10 -0500

    setup dark theme with mui

--- src/components/App/App.js
+++ src/components/App/App.js
@@ -1,23 +1,50 @@
-import {HashRouter as Router, Route} from 'react-router-dom';
-import './App.css';
-import MovieList from '../MovieList/MovieList'
+// React Router imports
+import { HashRouter as Router, Route } from 'react-router-dom';
+import { ThemeProvider, createTheme } from '@mui/material/styles';
+import { CssBaseline, Typography } from '@mui/material';
+import TheatersIcon from '@mui/icons-material/Theaters';
+import MovieList from '../MovieList/MovieList';
+import MovieDetails from '../MovieDetails/MovieDetails';
+import AddMovie from '../AddMovie/AddMovie';
+
+// MUI dark theme
+const darkTheme = createTheme({
+  palette: {
+    mode: 'dark',
+  },
+});
 
 function App() {
   return (
-    <div className="App">
-      <h1>The Movies Saga!</h1>
-      <Router>        
-        <Route path="/" exact>
-          <MovieList />
-        </Route>
-        
-        {/* Details page */}
+    <ThemeProvider theme={darkTheme}>
+      <CssBaseline />
+      <div className="App">
+        <Typography variant="h1" align="center">
+          <TheatersIcon fontSize="large" color="secondary" /> 
+          the movies saga 
+          <TheatersIcon fontSize="large" color="secondary" />
+        </Typography>
 
-        {/* Add Movie page */}
-      </Router>
-    </div>
+        <Router>
+          {/* route to Home */}
+          <Route path="/" exact>
+            <MovieList />
+          </Route>
+
+          {/* Route to Details page */}
+          <Route path="/details/:id">
+            <MovieDetails />
+          </Route>
+
+          {/* Route to Add Movie page */}
+          <Route path="/add">
+            <AddMovie />
+          </Route>
+        </Router>
+      </div>
+    </ThemeProvider>
   );
 }
 
-
 export default App;
+
