3c38655e6fe41863f15b586ac479e15c05799c51
Said Said <ssaid20@icloud.com>
2023-10-08 19:37:50 -0500

    implemented reducers for movies, details and genre

---
+++ src/redux/reducers/_root.reducer.js
@@ -0,0 +1,40 @@
+import { combineReducers } from "redux";
+
+
+// to store movies
+const movies = (state = [], action) => {
+    switch (action.type) {
+        case 'SET_MOVIES':
+            return action.payload;
+        default:
+            return state;
+    }
+}
+
+// to store the movie genres
+const genres = (state = [], action) => {
+    switch (action.type) {
+        case 'SET_GENRES':
+            return action.payload;
+        default:
+            return state;
+    }
+}
+
+// to store details on request from saga
+const details = (state = [], action) => {
+    switch (action.type) {
+        case 'SET_DETAILS':
+            return action.payload;
+        default:
+            return state;
+    }
+}
+
+const rootReducer = combineReducers({
+    movies,
+    genres,
+    details
+})
+
+export default rootReducer;
